d131e58f532946d95525f5b58cca7985e99491ac
Darupong Chouypu <darupong007@gmail.com>
2022-11-29 23:07:47 +0700

    style: fix layout about me

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,27 +1,70 @@
-import React from 'react'
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
 import Image from "next/image";
 
-import logo from "../public/img/myimg.png"
+import logo from "../public/img/myimg.png";
 
 const AboutMe = () => {
-    return (
-        <div>
-            <div className={`md:py-[100px] marker:min-h-[564px] md:min-h-screen`}>
-                <div className={`flex flex-col items-center justify-center`}>
-                    <p className=" font-bold text-[80px]">About Me</p>
+  return (
+    <div className={` py-[50px] container mx-auto`}>
+      <div className="mockup-window border bg-base-300">
+        <div className="flex justify-center px-4 py-16 bg-base-200">
+          <div className="flex flex-col">
+            <div className="flex md:flex-col flex-row justify-center items-center space-y-6 lg:px-10 px-4 md:py-14 py-6">
+              <div>
+                <div className="row-span-3 flex items-center justify-center">
+                  <Image
+                    src={logo}
+                    className="md:h-[300px] md:w-[300px] h-[10px] w-[10px]"
+                    alt="Flowbite Logo"
+                  />
                 </div>
-                <div className="grid grid-rows-3 grid-flow-col gap-4 pt-[50px]">
-                    <div className="row-span-3 flex items-center justify-end">
-                        <Image src={logo} className="h-[300px] w-[300px]" alt="Flowbite Logo" />
-                    </div>
-                    <div className="col-span-2 flex items-center justify-center">
-                        <p className=" font-bold text-[20px]"> I'm Darupong Chouypu <br /> A Web Developer and AR Filter. </p>
-                    </div>
-                    <div className="row-span-2 col-span-2 flex items-center justify-center">03</div>
+              </div>
+              <div>
+                <div className="mockup-code mx-[100px]">
+                  <pre data-prefix="$">
+                    <code>npm i 'About Me'</code>
+                  </pre>
+                  <pre data-prefix=">" className="text-warning">
+                    <code>✓ Name: Darupong Chouypu</code>
+                  </pre>
+                  <pre data-prefix=">" className="text-warning">
+                    <code>✓ Age: 23 y</code>
+                  </pre>
+                  <pre data-prefix=">" className="text-warning">
+                    <code>✓ Education: Rangsit University</code>
+                  </pre>
+                  <pre data-prefix=">" className="text-warning">
+                    <code>✓ Major: Computer Game Multimedia</code>
+                  </pre>
+                  <pre data-prefix=">" className="text-success">
+                    <code>✓ Position: Software Programmer</code>
+                  </pre>
+                  <pre data-prefix=">" className="text-success">
+                    <code>✓ Work Experiance: 1+</code>
+                  </pre>
+                  <pre data-prefix="$" className="">
+                    <code>Happy hacking!</code>
+                  </pre>
                 </div>
+              </div>
+              <div>
+              <div className="mockup-code md:mx-[100px] mx-[20px]">
+                <pre data-prefix=">" className="px-[20px]">
+                  <code>
+                    I work as a Software Programmer and Filter Effect Creater and I am interested in learning about Frontend Developer,
+                    Mobile Appication from React Native and Android Studio. I've previously worked on game development, Filter Creater from 
+                    Instagram and Tiktok, Virtual tour on website and Augmented reality on website
+                    </code>
+                </pre>
+              </div>
+              </div>
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,12 +4,14 @@ import css from '../styles/HeroSection.module.css'
 const HeroSection = () => {
     return (
         <div className={`${css.bgTitle} `}>
+            <div className={` container md:mx-auto`}>
             <div className={`md:py-[100px] marker:min-h-[564px] md:min-h-screen`}>
-                <div className={` pt-[200px] flex flex-col items-center justify-center`}>
-                    <p className=" font-bold text-[100px] text-white">Hi Everyone!</p>
-                    <p className=" font-bold text-[50px] text-gray-500 dark:text-white">My name is <span className={` text-white dark:text-gray-500`}>Darupong Chouypu</span></p>
-                    <p className={`${css.animated} font-bold text-[50px] pt-[10px]`}>Frontend Developer</p>
+                <div className={` py-[200px] flex flex-col items-center justify-center`}>
+                    <p className=" font-bold md:text-[100px] text-[30px] text-white">Hi Everyone!</p>
+                    <p className=" font-bold md:text-[50px] text-[20px] text-gray-500 dark:text-white">My name is <span className={` text-white dark:text-gray-500`}>Darupong Chouypu</span></p>
+                    <p className={`${css.animated} font-bold md:text-[50px] text-[20px] pt-[10px]`}>Frontend Developer</p>
                 </div>
+            </div>
             </div>
         </div>
     )
