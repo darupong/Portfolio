4914b8259e593c7ec185e66f5e6134c8056dcb35
Neung Darupong <darupong@pirsquare.net>
2023-06-23 15:51:44 +0700

    feat:tabs experience

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -105,15 +105,15 @@ const AboutMe = () => {
                 <h1>
                   I work as a{" "}
                   <span className=" text-[#36D399]">Front-end Developer</span>{" "}
-                  and{" "}
-                  <span className=" text-[#36D399]">Content Creater</span>{" "}
+                  and <span className=" text-[#36D399]">Content Creater</span>{" "}
                   and I am interested in learning about Frontend Developer,
                   Mobile Appication from React Native.
                 </h1>
                 <h1>
-                  I've previously worked on <span className=" text-[#36D399]">Game Development</span> , Filter Creater
-                  from Instagram or Tiktok , Virtual tour on website and
-                  Augmented reality on website.
+                  I've previously worked on{" "}
+                  <span className=" text-[#36D399]">Game Development</span> ,
+                  Filter Creater from Instagram or Tiktok , Virtual tour on
+                  website and Augmented reality on website.
                 </h1>
               </div>
             </div>
